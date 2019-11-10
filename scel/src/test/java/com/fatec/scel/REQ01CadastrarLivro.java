4d8d12ee1c47f6137e727ff19e5e616a400712cc
HP <HP@DV6-6170>
2019-11-10 00:43:41 -0200

    Refatoracao do casos de teste

--- scel/src/test/java/com/fatec/scel/REQ01CadastrarLivro.java
+++ scel/src/test/java/com/fatec/scel/REQ01CadastrarLivro.java
@@ -1,5 +1,6 @@
 package com.fatec.scel;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.Set;
 import javax.validation.ConstraintViolation;
@@ -26,37 +27,50 @@ public class REQ01CadastrarLivro {
 	 * Verificar o comportamento da classe LivroRepository
 	 */
 	@Test
-	public void CT01CadastrarLivroComSucesso() {
+	public void CT01CadastrarLivroComSucesso_valida_comportamento() {
 		// dado que o isbn nao esta cadastrado
 		repository.deleteAll();
 		// quando o usurio inclui as informacoes obrigatorias e confirma a operacao
 		Livro livro = new Livro("3333", "Teste de Software", "Delamaro");
 		repository.save(livro);
-		// entao
+		// entao a quantidade de livros no repositorio será 1
 		assertEquals(1, repository.count());
+
+	}
+	@Test
+	public void CT02CadastrarLivroComSucesso() {
+
+		// dado que o isbn nao esta cadastrado
+		repository.deleteAll();
+		// quando o usurio inclui as informacoes obrigatorias e confirma a operacao
+		Livro livro1 = new Livro("3333", "Teste de Software", "Delamaro");
+		repository.save(livro1);
+		// entao o estado do objeto obtido é igual ao objeto cadastrado 
+		Livro livro2 = repository.findByIsbn("3333");
+		assertTrue (livro1.equals(livro2));
+	}
+	@Test
+	public void CT03CadastrarLivroComSucesso_verificacao_de_violacao() {
+		validatorFactory = Validation.buildDefaultValidatorFactory();
+		validator = validatorFactory.getValidator();
+		// dado que as informações obrigatorias foram incluidas
+		Livro livro = new Livro("3333", "Teste de Software", "Delamaro");
+		// quando a verificacao de violacao for acionada
+		Set<ConstraintViolation<Livro>> violations = validator.validate(livro);
+		// entao nenhuma violacao sera detectada
+		assertTrue(violations.isEmpty());
 	}
 
 	@Test
-	public void CT02CadastrarLivroComSucesso_dados_validos() {
+	public void CT04CadastrarLivroCom_TituloInvalido_verificacao_de_violacao() {
 		validatorFactory = Validation.buildDefaultValidatorFactory();
 		validator = validatorFactory.getValidator();
-		// given:
-		Livro livro = new Livro("3333", "Teste de Software", "Delamaro");
-		// when:
+		// dado que o titulo do livro esta invalido
+		Livro livro = new Livro("3333", "", "Delamaro");
+		// quando a verificacao de violacao for acionada
 		Set<ConstraintViolation<Livro>> violations = validator.validate(livro);
-		// then:
-		assertTrue(violations.isEmpty());
+		// entao a mensagem o titulo deve ser preenchido será enviada
+		assertEquals("O titulo deve ser preenchido", violations.iterator().next().getMessage());
 	}
-	@Test
-	public void CT03DeveDetectarTituloInvalido() {
-	validatorFactory = Validation.buildDefaultValidatorFactory();
-	validator = validatorFactory.getValidator();
-	// dado que o titulo do livro esta invalido
-	Livro livro = new Livro("3333", "", "Delamaro");
-	// when:
-	Set<ConstraintViolation<Livro>> violations = validator.validate(livro);
-	// then:
-	assertEquals(violations.size(), 1);
-	assertEquals("O titulo deve ser preenchido", violations.iterator().next().getMessage());
-	}
+
 }
--- scel/src/test/java/com/fatec/scel/REQ03ExcluirLivro.java
+++ scel/src/test/java/com/fatec/scel/REQ03ExcluirLivro.java
@@ -17,14 +17,14 @@ class REQ03ExcluirLivro {
 	static Livro livro;
 	@Test
 	public void CT01ExcluirLivroComSucesso() {
-	// dado que o isbn nao esta cadastrado
+	// dado que o livro esta cadastrado
 	repository.deleteAll();
-	// quando o usurio inclui as informacoes obrigatorias e confirma a operacao
 	Livro livro = new Livro("3333", "Teste de Software", "Delamaro");
 	repository.save(livro);
+	//quando o usuario exclui o livro
 	Livro ro = repository.findByIsbn("3333");
 	repository.deleteById(ro.getId());
-	// entao o sistema valida as informações E envia uma mensagem de livro cadastrado com sucesso
+	// entao o livro é deletado da base
 	assertThat(repository.findByIsbn("3333")).isEqualTo(null);
 	}
 }
